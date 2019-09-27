eeda58f3105a23e0e90fd0a306e4cc1a6e0ccfbb
IIIBlackhartIII <blackhart1996@gmail.com>
2019-09-27 18:01:29 -0400

    Create FamiPong.ino
    Created Initial Arudino INO

---
+++ FamiPong.ino
@@ -0,0 +1,23 @@
+/******************************************************************
+ Created with PROGRAMINO IDE for Arduino - 27.09.2019 17:58:41
+ Project     : FamiPong Domination  
+ Libraries   :
+ Author      : Jeffrey "Blackhart" Hepbun
+ Description : Physical pong table for FamiLab's booth at MakerFaire Orlando 2019; paddles are driven by stepper motors on linear rails, scoring system run by IR break sensors.
+******************************************************************/
+
+
+
+void setup()
+{
+
+    // write your setup code here, to run once
+
+}
+
+void loop()
+{
+
+    // write your main code here, to run repeatedly
+
+}
