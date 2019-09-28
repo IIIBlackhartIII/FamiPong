3abfe1e5b5b622552d9bcc659140f7f1d87787f5
IIIBlackhartIII <blackhart1996@gmail.com>
2019-09-28 00:07:06 -0400

    Update FamiPong.ino
    broke stepper motor controls into a custom function, easier to call in the main loop, also defined constants for the steppers including the stepType, numSteps for each call of the function, and stepperSpeed RPM.

--- FamiPong.ino
+++ FamiPong.ino
@@ -6,37 +6,65 @@
 ******************************************************************/
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include <utility/Adafruit_PWMServoDriver.h>
 
 #define DEBUG false
 
-///STEPPER MOTORS//
+///STEPPER MOTORS///
 //Motors are wired into the terminals Red, Yellow, (skip GND), Green, Grey (or Brown)
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //create stepper motor object
-int StepperSpeed = 50; //set stepper motors speed in RPM
-
-void setup()
-{
-    ///STEPPER MOTORS//
-    Adafruit_StepperMotor stepperMotor1 = AFMS.getStepper(200,1); //Player 1 Nema17 motor, 200 steps per revolution, connected to Port1 (terminals M1+M2)
-    Adafruit_StepperMotor stepperMotor2 = AFMS.getStepper(200,2); //Player 2 Nema17 motor, 200 steps per revolution, connected to Port1 (terminals M3+M4)
-    stepperMotor1.setSpeed(StepperSpeed); //set motor to run at defined speed
-    stepperMotor2.setSpeed(StepperSpeed); //set motor to run at defined speed
-}
-
-void loop()
-{
-
-   ///STEPPER MOTORS///
-   /*
+Adafruit_StepperMotor *stepperMotor1 = AFMS.getStepper(200,1); //Player 1 Nema17 motor, 200 steps per revolution, connected to Port1 (terminals M1+M2)
+Adafruit_StepperMotor *stepperMotor2 = AFMS.getStepper(200,2); //Player 2 Nema17 motor, 200 steps per revolution, connected to Port1 (terminals M3+M4)
+/*
    Step types are "SINGLE", "DOUBLE", "INTERLEAVE", or "MICROSTEP":
       "Single" means single-coil activation
       "Double" means 2 coils are activated at once (for higher torque)
       "Interleave" means that it alternates between single and double to get twice the resolution (but of course its half the speed).
       "Microstepping" is a method where the coils are PWM'd to create smooth motion between steps.
    */
-   stepperMotor1.step(100, FORWARD, DOUBLE); //#Steps, Direction, Step Type
-   stepperMotor2.step(100, FORWARD, DOUBLE); //#Steps, Direction, Step Type
-   
-   delay(1000);
+#define stepType DOUBLE
+#define numSteps 100
+#define StepperSpeed 500 //set stepper motors speed in RPM
+
+
+void setup()
+{
+  ///STEPPER MOTORS///
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+  
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  
+  stepperMotor1->setSpeed(StepperSpeed); //set motor to run at defined speed
+  stepperMotor2->setSpeed(StepperSpeed); //set motor to run at defined speed
+}
+
+
+void loop()
+{
+
+   ///STEPPER MOTORS///
+   moveStepper("Player2","Backward");
+}
+
+
+/////CUSTOM FUNCTIONS/////
+
+void moveStepper(String motor, String motorDir)
+{
+  if (motor == "Player1"){
+    if (motorDir == "Forward"){
+      stepperMotor1->step(numSteps, FORWARD, stepType); //#Steps, Direction, Step Type
+    }
+    else{
+      stepperMotor1->step(numSteps, BACKWARD, stepType); //#Steps, Direction, Step Type
+    }
+  }
+  else{
+    if (motorDir == "Forward"){
+      stepperMotor2->step(numSteps, FORWARD, stepType); //#Steps, Direction, Step Type
+    }
+    else{
+      stepperMotor2->step(numSteps, BACKWARD, stepType); //#Steps, Direction, Step Type
+    }
+  }
 }
