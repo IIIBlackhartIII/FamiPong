17f0cbaac6c574eeb6b874f5cf2dff2708677481
IIIBlackhartIII <blackhart1996@gmail.com>
2019-09-27 23:15:36 -0400

    Update FamiPong.ino
    adding stepper motor setup, demo motor code

--- FamiPong.ino
+++ FamiPong.ino
@@ -1,23 +1,42 @@
 /******************************************************************
  Created with PROGRAMINO IDE for Arduino - 27.09.2019 17:58:41
  Project     : FamiPong Domination  
- Libraries   :
  Author      : Jeffrey "Blackhart" Hepbun
  Description : Physical pong table for FamiLab's booth at MakerFaire Orlando 2019; paddles are driven by stepper motors on linear rails, scoring system run by IR break sensors.
 ******************************************************************/
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include <utility/Adafruit_PWMServoDriver.h>
 
+#define DEBUG false
 
+///STEPPER MOTORS//
+//Motors are wired into the terminals Red, Yellow, (skip GND), Green, Grey (or Brown)
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //create stepper motor object
+int StepperSpeed = 50; //set stepper motors speed in RPM
 
 void setup()
 {
-
-    // write your setup code here, to run once
-
+    ///STEPPER MOTORS//
+    Adafruit_StepperMotor stepperMotor1 = AFMS.getStepper(200,1); //Player 1 Nema17 motor, 200 steps per revolution, connected to Port1 (terminals M1+M2)
+    Adafruit_StepperMotor stepperMotor2 = AFMS.getStepper(200,2); //Player 2 Nema17 motor, 200 steps per revolution, connected to Port1 (terminals M3+M4)
+    stepperMotor1.setSpeed(StepperSpeed); //set motor to run at defined speed
+    stepperMotor2.setSpeed(StepperSpeed); //set motor to run at defined speed
 }
 
 void loop()
 {
 
-    // write your main code here, to run repeatedly
-
+   ///STEPPER MOTORS///
+   /*
+   Step types are "SINGLE", "DOUBLE", "INTERLEAVE", or "MICROSTEP":
+      "Single" means single-coil activation
+      "Double" means 2 coils are activated at once (for higher torque)
+      "Interleave" means that it alternates between single and double to get twice the resolution (but of course its half the speed).
+      "Microstepping" is a method where the coils are PWM'd to create smooth motion between steps.
+   */
+   stepperMotor1.step(100, FORWARD, DOUBLE); //#Steps, Direction, Step Type
+   stepperMotor2.step(100, FORWARD, DOUBLE); //#Steps, Direction, Step Type
+   
+   delay(1000);
 }
