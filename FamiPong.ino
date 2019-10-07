3d7d58b5c8ab650b8951da64bca1dd9461f2809d
IIIBlackhartIII <blackhart1996@gmail.com>
2019-10-07 03:55:11 -0400

    Update FamiPong.ino
    Attempts to increase motor drive speed.

--- FamiPong.ino
+++ FamiPong.ino
@@ -1,7 +1,7 @@
 /******************************************************************
  Created with PROGRAMINO IDE for Arduino - 27.09.2019 17:58:41
  Project     : FamiPong Domination  
- Author      : Jeffrey "Blackhart" Hepbun
+ Author      : Jeffrey "Blackhart" Hepburn
  Description : Physical pong table for FamiLab's booth at MakerFaire Orlando 2019; paddles are driven by stepper motors on linear rails, scoring system run by IR break sensors.
 ******************************************************************/
 #include <Wire.h>
@@ -19,11 +19,15 @@ Adafruit_StepperMotor *stepperMotor2 = AFMS.getStepper(200,2); //Player 2 Nema17
       "Single" means single-coil activation
       "Double" means 2 coils are activated at once (for higher torque)
       "Interleave" means that it alternates between single and double to get twice the resolution (but of course its half the speed).
-      "Microstepping" is a method where the coils are PWM'd to create smooth motion between steps.
+      "Microstep" is a method where the coils are PWM'd to create smooth motion between steps.
    */
 #define stepType DOUBLE
-#define numSteps 100
+#define numSteps 250
 #define StepperSpeed 500 //set stepper motors speed in RPM
+
+///CONTROLLERS///
+
+/* TODO defining microswitch inputs to go here */
 
 
 void setup()
@@ -33,6 +37,7 @@ void setup()
   Serial.println("Stepper test!");
   
   AFMS.begin();  // create with the default frequency 1.6KHz
+  Wire.setClock(400000); //set speed of I2C bus, increases rate of polling to stepper sheild
   
   stepperMotor1->setSpeed(StepperSpeed); //set motor to run at defined speed
   stepperMotor2->setSpeed(StepperSpeed); //set motor to run at defined speed
@@ -43,7 +48,16 @@ void loop()
 {
 
    ///STEPPER MOTORS///
-   moveStepper("Player2","Backward");
+   
+   //Player1 Controller Input 
+   moveStepper("Player1","Forward"); 
+   delay(2000);
+   moveStepper("Player1","Backward"); 
+   delay(2000);
+   
+   //Player2 Controller Input
+   //moveStepper("Player2","Forward"); 
+   //moveStepper("Player2","Backward"); 
 }
 
 
